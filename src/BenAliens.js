ddc7c71df4ac51542c6c91d491dc5993f57ae0ee
Nini O.X <112957015+Vultrex111@users.noreply.github.com>
2024-01-15 17:38:13 +0100

    Add files via upload

---
+++ src/BenAliens.js
@@ -0,0 +1,96 @@
+export const characters = [
+  // Iron Man
+  {
+    alias: "Alien X",
+    img1: "src/assets/Aliens/Alien_X_2.webp",
+    homeworld: "Forge of Creation",
+    abilities: [
+      "Genius-level intellect",
+      "Mental Reality Warping",
+      "Unlimited Powers",
+    ],
+    description:
+      "Tony Stark, also known as Iron Man, is a brilliant inventor, philanthropist, and industrialist. He uses his genius-level intellect and a highly advanced powered armor suit to fight for justice and protect the world from various threats. Stark is a charismatic playboy turned superhero who combines his technological prowess with unwavering determination to uphold the values of heroism and innovation.",
+  },
+  {
+    alias: "Diamond Head",
+    abilities: ["Crystallokinesis"],
+    img1: "src/assets/Aliens/Diamondhead_in_UAF.PNG.png",
+    homeworld: "Petropia",
+    description:
+      "Being a silicon-based life form, Diamondhead's body is composed entirely of pale green/blue crystals, which are of a very tough and durable diamond-like substance.[MW 1] He sports four crystal shards on his back and has a sharp head.",
+  },
+  {
+    alias: "Big Chill",
+    abilities: ["Cryokinesis", "Freezing Breath"],
+    img1: "src/assets/Aliens/HD-wallpaper-big-chill-ben-10-benten-thumbnail.jpg",
+    homeworld: "Kylmyys",
+
+    description:
+      "James 'Rhodey' Rhodes, also known as War Machine, is a highly skilled pilot and a military expert. He operates a heavily armed powered armor suit that enhances his strength and combat capabilities. With a strong sense of duty and loyalty to his allies, Rhodey plays a crucial role in the Avengers' efforts to maintain peace and protect the world.",
+  },
+  {
+    alias: "Way Big",
+    abilities: ["Cosmic Rays", "Enhanced Strength"],
+    img1: "src/assets/Aliens/HD-wallpaper-waybig-ben-10-benten-thumbnail.jpg",
+    homeworld: "To'kustar",
+
+    description:
+      "Riri Williams, known as Ironheart, is a prodigious young inventor with a genius-level intellect. She has designed her own advanced powered armor suit, which she uses to fight against threats as a member of the Young Avengers. Riri's intelligence, determination, and innovative spirit make her a rising star in the superhero community.",
+  },
+
+  // Spider-Man
+  {
+    alias: "Heat Blast",
+    abilities: ["Pyrokinesis"],
+    img1: "src/assets/Aliens/HD-wallpaper-ben-10-10-ben-benjamin-classic-fire-fuego-kirby-old-tennyson-viejo-thumbnail.jpg",
+    homeworld: "Pyros",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Heatblast is a plasma-based life-form whose body is composed of a super hot inner plasma body covered by dark reddish brown volcanic rocks.",
+  },
+
+  {
+    alias: "Four Arms",
+    abilities: ["Enhanced Strength"],
+    img1: "src/assets/Aliens/Four_arms_os_render.webp",
+    homeworld: "Khoros",
+    description:
+      "Four Arms is a humanoid alien that is ten feet tall.[merch 2][note 1] He has well-developed muscles, two pairs of four-fingered arms, and red skin. ",
+  },
+
+  {
+    alias: "RipJaws",
+    abilities: ["Strong Bite Force"],
+    img1: "src/assets/Aliens/Ripjaws.webp",
+    homeworld: "Piscciss",
+    description:
+      "Ripjaws shares a number of traits with Earth's aquatic life, such as an alligator, an eel, a leech,[merch 1] an anglerfish, and a shark. He can change between a fishtail with triangular lines or legs at will and has a phosphorescent light on his head.",
+  },
+  {
+    alias: "Grey Matter",
+    abilities: ["Enhanced Intelligence"],
+    img1: "src/assets/Aliens/Grey_Matter.webp",
+    homeworld: "Galvan Prime",
+    description:
+      "Grey Matter is a grey-skinned, frog-like, bipedal alien that is only six inches tall[4][5] with large green eyes, which have rectangular pupils and horizontal eyelids.",
+  },
+  {
+    alias: "Ghost Freak",
+    abilities: ["Intangibility"],
+    img1: "src/assets/Aliens/Ghostfreak_ov_official.webp",
+    homeworld: "Anur Phaetos",
+    description:
+      "Ghostfreak is a phantom/ghost-like alien with a gaseous appearance and claw-like hands, and grey skin with black lines running all over him. His skin is removable with black and white striped tentacles underneath it.",
+  },
+  {
+    alias: "Eye Guy",
+    abilities: ["Ophthalmokinesis", "Energy Beams"],
+    img1: "src/assets/Aliens/EyeGuy_Model.webp",
+    homeworld: "Sightra",
+    //description:
+     // "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+];
+
+console.log(characters);
